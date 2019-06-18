6b6ee9dd9c5f80f24723eb800cedbe041e4a5366
martinamorris <morrism@uw.edu>
2019-06-18 09:43:22 -0400

    update tergm tutorial docs Sunbelt 2019

--- docs/tergm_tutorial.R
+++ docs/tergm_tutorial.R
@@ -1,88 +1,131 @@
 ## ----setup, include = FALSE----------------------------------------------
 library(knitr)
-knitr::opts_chunk$set(comment = NA)
+knitr::opts_chunk$set(cache=F, comment=NA, fig.align='center')
+
+
+## ----dev, child = '../statnetDevTeam.Rmd'--------------------------------
+
+
+
+
+## ----project, child = '../statnetProject.Rmd'----------------------------
+
+
+
 
 ## ----eval = FALSE--------------------------------------------------------
-## install.packages('statnet')
-## install.packages('tsna')
-## install.packages('ndtv')
-## install.packages('htmlwidgets')
+install.packages('statnet')
+install.packages('ndtv')
+install.packages('htmlwidgets')
+install.packages('latticeExtra')
 
-## ----eval = FALSE--------------------------------------------------------
-## install.packages('latticeExtra')
 
 ## ----results = 'hide', message = FALSE, warning = FALSE------------------
 library(statnet)
-library(tsna)
-library(latticeExtra)
 library(ndtv)
 library(htmlwidgets)
+library(latticeExtra)
 
-## ----eval = FALSE--------------------------------------------------------
-## sessionInfo()
+
+## ---- cache = FALSE------------------------------------------------------
+sessionInfo()
+
 
 ## ------------------------------------------------------------------------
 set.seed(0)
+
 
 ## ------------------------------------------------------------------------
 data(samplk)
 ls()
 
+
 ## ------------------------------------------------------------------------
 samplist <- list(samplk1,samplk2,samplk3)
 sampdyn <- networkDynamic(network.list = samplist)
 
+
 ## ------------------------------------------------------------------------
-sampdyn # note it says 4 time points
-network.collapse(sampdyn, at = 4) # this is an artifact of discrete time
+sampdyn
+
 
 ## ---- eval = F-----------------------------------------------------------
-## vignette("networkDynamic")
+vignette("networkDynamic")
+
 
 ## ------------------------------------------------------------------------
-par(mfrow = c(1,3))
-plot(network.extract(sampdyn, at = 0), main = "Time 1", displaylabels = T)
-plot(network.extract(sampdyn, at = 1), main = "Time2", displaylabels = T)
-plot(network.extract(sampdyn, at = 2), main = "Time3", displaylabels = T)
+par(mfrow = c(2,2), oma=c(1,1,1,1), mar=c(4,1,1,1))
+plot(network.extract(sampdyn, at = 0), main = "Time 1", 
+     displaylabels = T, label.cex = 0.6, vertex.cex = 2, pad = 0.5)
+plot(network.extract(sampdyn, at = 1), main = "Time2", 
+     displaylabels = T, label.cex = 0.6, vertex.cex = 2, pad = 0.5)
+plot(network.extract(sampdyn, at = 2), main = "Time3", 
+     displaylabels = T, label.cex = 0.6, vertex.cex = 2, pad = 0.5)
+plot(sampdyn, main = "Collapsed", 
+     displaylabels = T, label.cex = 0.6, vertex.cex = 2, pad = 0.5)
+
 
 ## ------------------------------------------------------------------------
 tSnaStats(sampdyn,"degree") # Changes in degree centrality
 
+
 ## ------------------------------------------------------------------------
 tErgmStats(sampdyn, "~ edges+triangle") # Notice the increase in triangles
 
+
 ## ------------------------------------------------------------------------
-tp <- tPath(sampdyn, 1, direction = 'fwd')
+# who is v1?
+get.vertex.attribute(sampdyn, "vertex.names")[1]
+
+# who is in v1's FRS?
+tp <- tPath(sampdyn, v=1, direction = 'fwd')
 print(tp)
-plotPaths(sampdyn, tp)
+par(mfrow=c(1,2))
+coords <- plot(tp, main="Forward Reachable Set from v1", cex.main=.8)
+plotPaths(sampdyn, tp, 
+          coord = coords,
+          main = "Overlaid on collapsed Network",
+          label.cex=.8, cex.main=.8)
+
 
 
 ## ------------------------------------------------------------------------
 table(edgeDuration(sampdyn, mode = 'duration', subject = 'spells')) # Note bimodality
 
+
 ## ------------------------------------------------------------------------
 library(ndtv)
 data(short.stergm.sim)
 
+
 ## ---- eval = FALSE, message = FALSE,results = 'hide',cache = FALSE-------
-## render.d3movie(short.stergm.sim,
-##                plot.par=list(displaylabels=T))
+render.d3movie(short.stergm.sim, 
+               plot.par=list(displaylabels=T))
+
 
 ## ---- message = FALSE,cache = FALSE--------------------------------------
 render.d3movie(short.stergm.sim,
                plot.par=list(displaylabels=T),
-               output.mode = 'htmlWidget')
+               output.mode = 'htmlWidget') # using htmlwidgets package here
+
 
 ## ----message = FALSE,cache = TRUE----------------------------------------
 proximity.timeline(short.stergm.sim,default.dist = 6,
           mode = 'sammon',labels.at = 17,vertex.cex = 4)
 
+
 ## ----eval = FALSE--------------------------------------------------------
-##         stergm(my.network,                 #do not run this
-##             formation =  ~edges+kstar(2),
-##             dissolution =  ~edges+triangle,
-##             estimate =  `insert method`
-##         )
+        ergm(my.network ~ edges + gwesp(0, fixed=T))     #do not run this
+           
+
+
+## ----eval = FALSE--------------------------------------------------------
+        stergm(my.network,                               #do not run this
+            formation =  ~ edges + nodefactor('age'),
+            dissolution =  ~ edges + gwesp(0, fixed=T),
+            estimate =  `insert method`
+        )
+
 
 ## ----results = "hide", message = FALSE-----------------------------------
 samp.fit <- stergm(samplist,
@@ -92,8 +135,10 @@ samp.fit <- stergm(samplist,
 	times = c(1:3)
 	)
 
+
 ## ------------------------------------------------------------------------
 summary(samp.fit)
+
 
 ## ----results = "hide", message = FALSE-----------------------------------
 samp.fit.2 <- stergm(samplist,
@@ -103,11 +148,14 @@ samp.fit.2 <- stergm(samplist,
   times = 1:2
 	)
 
+
 ## ------------------------------------------------------------------------
 summary(samp.fit.2)
 
+
 ## ------------------------------------------------------------------------
 theta.diss <- log(9)
+
 
 ## ----results = 'hide',message = FALSE,warning = FALSE--------------------
 data(florentine)
@@ -122,17 +170,21 @@ stergm.fit.1 <- stergm(flobusiness,
 	)
 dev.off()
 
+
 ## ----eval = FALSE--------------------------------------------------------
-## mcmc.diagnostics(stergm.fit.1)
+mcmc.diagnostics(stergm.fit.1)
+
 
 ## ------------------------------------------------------------------------
 stergm.fit.1
 names(stergm.fit.1)
 summary(stergm.fit.1)
 
+
 ## ------------------------------------------------------------------------
-stergm.sim.1 <- simulate.stergm(stergm.fit.1, nsim = 1, 
+stergm.sim.1 <- simulate(stergm.fit.1, nsim = 1, 
     time.slices = 1000)
+
 
 ## ---- message = FALSE, cache = FALSE-------------------------------------
 wealthsize <- log(get.vertex.attribute(flobusiness, "wealth")) * 2/3
@@ -155,38 +207,49 @@ render.d3movie(stergm.sim.1,
                plot.par = plot.par,
                output.mode = 'htmlWidget')
 
+
 ## ------------------------------------------------------------------------
-summary(flobusiness~edges+gwesp(0,fixed = T))
-colMeans(attributes(stergm.sim.1)$stats)
+cbind(model = summary(flobusiness~edges+gwesp(0,fixed = T)),
+      obs = colMeans(attributes(stergm.sim.1)$stats))
+
 
 ## ------------------------------------------------------------------------
 plot(attributes(stergm.sim.1)$stats)
 
+
 ## ------------------------------------------------------------------------
 plot(as.matrix(attributes(stergm.sim.1)$stats))
 
+
 ## ------------------------------------------------------------------------
+# create dataFrame for direct estimation
 stergm.sim.1.df <- as.data.frame(stergm.sim.1)
 names(stergm.sim.1.df)
 stergm.sim.1.df[1,]
-mean(stergm.sim.1.df$duration)
-mean(edgeDuration(stergm.sim.1, 
+
+cbind(directEst = mean(stergm.sim.1.df$duration),
+      ndEst = mean(edgeDuration(stergm.sim.1, 
                   mode = 'duration', 
-                  subject = 'spells'))
+                  subject = 'spells')))
+
 
 ## ------------------------------------------------------------------------
 theta.diss.100 <- log(99)
 
+
 ## ---- results = "hide", message = FALSE----------------------------------
 ergm.fit1 <- ergm(flobusiness ~ edges + gwesp(0, fixed = T))
+
 
 ## ------------------------------------------------------------------------
 summary(ergm.fit1)
 theta.form <- ergm.fit1$coef 
 theta.form
 
+
 ## ------------------------------------------------------------------------
 theta.form[1] <- theta.form[1] - theta.diss.100
+
 
 ## ------------------------------------------------------------------------
 stergm.sim.2 <- simulate(flobusiness, 
@@ -197,10 +260,15 @@ stergm.sim.2 <- simulate(flobusiness,
                          coef.diss = theta.diss.100,
                          time.slices = 50000)
 
+
 ## ------------------------------------------------------------------------
-summary(flobusiness~edges+gwesp(0,fixed = T))
-colMeans(attributes(stergm.sim.2)$stats)
+# first, recovery of the cross sectional statistics in the formation model
+cbind(observed = summary(flobusiness~edges+gwesp(0,fixed = T)),
+      simulated = colMeans(attributes(stergm.sim.2)$stats))
+
+plot(attributes(stergm.sim.2)$stats)
+
+# second, recovery of the tie duration
 stergm.sim.dm.2 <- as.data.frame(stergm.sim.2)
 mean(stergm.sim.dm.2$duration)
-plot(attributes(stergm.sim.2)$stats)
 
