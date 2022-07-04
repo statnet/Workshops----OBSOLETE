0b193896c56c7cede42c362175a00c0f01e4162f
David Hunter <dhunter@stat.psu.edu>
2022-07-04 08:51:38 -0400

    July 4 2022 updates to workshop-ergm/ergm_tutorial.R

--- ergm/ergm_tutorial.R
+++ ergm/ergm_tutorial.R
@@ -1,100 +1,92 @@
-## ----setup, include=FALSE----------------------------------------------------------------------------------
+## ----setup, include=FALSE------------------------------------------------------------
 library(knitr)
-knitr::opts_chunk$set(cache=F, comment=NA)
+knitr::opts_chunk$set(cache=T, comment=NA)
 
 
-## ----dev, child = '../statnetDevTeam.Rmd'------------------------------------------------------------------
-
-
-
-
-## ----project, child = '../statnetProject.Rmd'--------------------------------------------------------------
+## ----dev, child = 'common/statnet-dev-team.md'---------------------------------------
 
 
 
 
-## ----install-ergm,eval=FALSE-------------------------------------------------------------------------------
-## install.packages('ergm')
+## ----project, child = 'common/statnet-project.md'------------------------------------
 
 
-## ----loadPackage-------------------------------------------------------------------------------------------
+
+
+## ----install-ergm,eval=FALSE---------------------------------------------------------
+install.packages('ergm')
+
+
+## ----loadPackage---------------------------------------------------------------------
 library(ergm)
 
 
-## ----version-----------------------------------------------------------------------------------------------
+## ----version-------------------------------------------------------------------------
 packageVersion("ergm")
 
 
-## ----ergm-terms-help, eval=FALSE---------------------------------------------------------------------------
-## ?'ergm-terms'
+## ----homophily-terms-search----------------------------------------------------------
+search.ergmTerms(keyword='curved')
 
 
-## ----homophily-terms-search--------------------------------------------------------------------------------
-search.ergmTerms(keyword='homophily')
-
-
-## ----vignette----------------------------------------------------------------------------------------------
-vignette('ergm-term-crossRef')
-
-
-## ----pajek-------------------------------------------------------------------------------------------------
+## ----pajek---------------------------------------------------------------------------
 ?read.paj
 ?read.paj.simplify
 ?loading.attributes
 
 
-## ----network-help------------------------------------------------------------------------------------------
+## ----network-help--------------------------------------------------------------------
 ?network
 
 
-## ----ergm-datasets-----------------------------------------------------------------------------------------
+## ----ergm-datasets-------------------------------------------------------------------
 data(package='ergm') # tells us the datasets in our packages
 
 
-## ----florentine-plots--------------------------------------------------------------------------------------
+## ----florentine-plots----------------------------------------------------------------
 set.seed(123) # The plot.network function uses random values
 data(florentine) # loads flomarriage and flobusiness data
-flomarriage # Look at the flomarriage network properties (uses `network`), esp. the vertex attributes
-par(mfrow=c(1,2)) # Setup a 2 panel plot
+flomarriage # Equivalent to print.network(flomarriage): Examine properties
+par(mfrow=c(1,2)) # Set up a 2-column (and 1-row) plot area
 plot(flomarriage, 
      main="Florentine Marriage", 
      cex.main=0.8, 
-     label = network.vertex.names(flomarriage)) # Plot the network
+     label = network.vertex.names(flomarriage)) # Equivalent to plot.network(...)
 wealth <- flomarriage %v% 'wealth' # %v% references vertex attributes
 wealth
 plot(flomarriage, 
-     vertex.cex=wealth/25, 
-     main="Florentine marriage by wealth", cex.main=0.8) # Plot the network with vertex size proportional to wealth
+     vertex.cex=wealth/25, # Make vertex size proportional to wealth attribute
+     main="Florentine marriage by wealth", cex.main=0.8) 
 
 
-## ----flomarriage-edges-------------------------------------------------------------------------------------
-summary(flomarriage ~ edges) # Look at the $g(y)$ statistic for this model
+## ----flomarriage-edges---------------------------------------------------------------
+summary(flomarriage ~ edges) # Calculate the edges statistic for this network
 flomodel.01 <- ergm(flomarriage ~ edges) # Estimate the model 
 summary(flomodel.01) # Look at the fitted model object
 
 
-## ----flomarriage-triangle, message = FALSE-----------------------------------------------------------------
+## ----flomarriage-triangle, message = FALSE-------------------------------------------
 set.seed(321)
-summary(flomarriage~edges+triangle) # Look at the g(y) stats for this model
-flomodel.02 <- ergm(flomarriage~edges+triangle) 
+summary(flomarriage~edges+triangle) # Look at the g(y) statistics for this model
+flomodel.02 <- ergm(flomarriage~edges+triangle) # Estimate the theta coefficients
 summary(flomodel.02)
 
 
-## ----ilogit------------------------------------------------------------------------------------------------
+## ----ilogit--------------------------------------------------------------------------
 plogis(coef(flomodel.02)[[1]] + (0:2) * coef(flomodel.02)[[2]])
 
 
-## ----flomarriage-ergm-object-------------------------------------------------------------------------------
+## ----flomarriage-ergm-object---------------------------------------------------------
 class(flomodel.02) # this has the class ergm
 
 names(flomodel.02) # the ERGM object contains lots of components.
 
 
-## ----ergm-object-coef--------------------------------------------------------------------------------------
+## ----ergm-object-coef----------------------------------------------------------------
 coef(flomodel.02) # you can extract/inspect individual components
 
 
-## ----flomarriage-wealth------------------------------------------------------------------------------------
+## ----flomarriage-wealth--------------------------------------------------------------
 summary(wealth) # summarize the distribution of wealth
 # plot(flomarriage, 
 #      vertex.cex=wealth/25, 
@@ -105,12 +97,12 @@ flomodel.03 <- ergm(flomarriage~edges+nodecov('wealth'))
 summary(flomodel.03)
 
 
-## ----faux-mesa-high----------------------------------------------------------------------------------------
+## ----faux-mesa-high------------------------------------------------------------------
 data(faux.mesa.high) 
 mesa <- faux.mesa.high
 
 
-## ----plot-mesa---------------------------------------------------------------------------------------------
+## ----plot-mesa-----------------------------------------------------------------------
 set.seed(1)
 mesa
 par(mfrow=c(1,1)) # Back to 1-panel plots
@@ -119,40 +111,40 @@ legend('bottomleft',fill=7:12,
        legend=paste('Grade',7:12),cex=0.75)
 
 
-## ----mesa-ergm---------------------------------------------------------------------------------------------
+## ----mesa-ergm-----------------------------------------------------------------------
 fauxmodel.01 <- ergm(mesa ~edges + 
                        nodefactor('Grade') + nodematch('Grade',diff=T) +
                        nodefactor('Race') + nodematch('Race',diff=T))
 summary(fauxmodel.01)
 
 
-## ----mesa-mixingmatrix-------------------------------------------------------------------------------------
+## ----mesa-mixingmatrix---------------------------------------------------------------
 table(mesa %v% 'Race') # Frequencies of race
 mixingmatrix(mesa, "Race")
 
 
-## ----mesa-summary------------------------------------------------------------------------------------------
+## ----mesa-summary--------------------------------------------------------------------
 summary(mesa ~edges  + 
           nodefactor('Grade') + nodematch('Grade',diff=T) +
           nodefactor('Race') + nodematch('Race',diff=T))
 
 
-## ----samplk------------------------------------------------------------------------------------------------
+## ----samplk--------------------------------------------------------------------------
 set.seed(2)
-data(samplk) 
-ls() # directed data: Sampson's Monks
+data(samplk) # directed data: Sampson's Monks
+ls() 
 samplk3
 plot(samplk3)
 summary(samplk3~edges+mutual)
 
 
-## ----samplk3-ergm, message = F-----------------------------------------------------------------------------
+## ----samplk3-ergm, message = F-------------------------------------------------------
 set.seed(3)
 sampmodel.01 <- ergm(samplk3~edges+mutual)
 summary(sampmodel.01)
 
 
-## ----missing-data------------------------------------------------------------------------------------------
+## ----missing-data--------------------------------------------------------------------
 set.seed(4)
 missnet <- network.initialize(10,directed=F) # initialize an empty net with 10 nodes
 missnet[1,2] <- missnet[2,7] <- missnet[3,6] <- 1 # add a few ties
@@ -172,7 +164,7 @@ summary(missnet~edges)
 summary(ergm(missnet~edges))
 
 
-## ----missnet-mod-------------------------------------------------------------------------------------------
+## ----missnet-mod---------------------------------------------------------------------
 missnet_bad <- missnet # create network with missing dyads set to 0
 missnet_bad[4,6] <- missnet_bad[4,9] <- missnet_bad[5,6] <- 0
 
@@ -181,7 +173,7 @@ summary(missnet_bad)
 summary(ergm(missnet_bad~edges))
 
 
-## ----flobusiness, message = F------------------------------------------------------------------------------
+## ----flobusiness, message = F--------------------------------------------------------
 set.seed(314159)
 summary(flobusiness~edges+degree(1))
 fit <- ergm(flobusiness~edges+degree(1))
@@ -189,39 +181,41 @@ summary(fit)
 mcmc.diagnostics(fit)
 
 
-## ----flobusiness-MCMCinterval, eval=FALSE------------------------------------------------------------------
-## set.seed(271828)
-## fit <- ergm(flobusiness~edges+degree(1),
-##             control=snctrl(MCMC.interval=1))
+## ----flobusiness-MCMCinterval, eval=FALSE--------------------------------------------
+set.seed(271828)
+fit <- ergm(flobusiness~edges+degree(1),
+            control=snctrl(MCMC.interval=1))
 
 
-## ----flomarriage-simulate----------------------------------------------------------------------------------
+## ----flomarriage-simulate------------------------------------------------------------
 set.seed(101)
 flomodel.03.sim <- simulate(flomodel.03,nsim=10)
+class(flomodel.03.sim) # Reveal the class of the object created
+summary(flomodel.03.sim) # quick summary of a network.list object
+attributes(flomodel.03.sim) # Reveal the various attributes of this network.list
 
-class(flomodel.03.sim) # what does this produce?
-summary(flomodel.03.sim) # quick summary
-attributes(flomodel.03.sim) # what's in this object?
 
-# are the simulated stats centered on the observed stats?
+## ----flomarriage-simulate2-----------------------------------------------------------
 rbind("obs"=summary(flomarriage~edges+nodecov("wealth")),
       "sim mean"=colMeans(attr(flomodel.03.sim, "stats"))) 
 
+
+## ----flomarriage-simulate3-----------------------------------------------------------
 # we can also plot individual simulations
-flomodel.03.sim[[1]]
-plot(flomodel.03.sim[[1]], 
-     label= flomodel.03.sim[[1]] %v% "vertex.names",
-     vertex.cex = (flomodel.03.sim[[1]] %v% "wealth")/25)
+flomodel.03.sim[[7]]
+plot(flomodel.03.sim[[7]], 
+     label= flomodel.03.sim[[7]] %v% "vertex.names",
+     vertex.cex = (flomodel.03.sim[[7]] %v% "wealth")/25)
 
 
-## ----flomarriage-gof---------------------------------------------------------------------------------------
+## ----flomarriage-gof-----------------------------------------------------------------
 set.seed(54321) # The gof function uses random values
 flomodel.03.gof <- gof(flomodel.03)
 flomodel.03.gof
 plot(flomodel.03.gof)
 
 
-## ----mesa-gof----------------------------------------------------------------------------------------------
+## ----mesa-gof------------------------------------------------------------------------
 set.seed(12345)
 mesamodel.02 <- ergm(mesa~edges)
 mesamodel.02.gof <- gof(mesamodel.02~degree + esp + distance, 
@@ -229,40 +223,38 @@ mesamodel.02.gof <- gof(mesamodel.02~degree + esp + distance,
 plot(mesamodel.02.gof)
 
 
-## ----magnolia----------------------------------------------------------------------------------------------
+## ----magnolia------------------------------------------------------------------------
 set.seed(10)
 data('faux.magnolia.high')
 magnolia <- faux.magnolia.high
 magnolia
 plot(magnolia, vertex.cex=.5)
-
-# Consider a simple model
-summary(magnolia~edges+triangle)
+summary(magnolia~edges+triangle) # Simple model for triad closure
 
 
-## ----magnolia-triangle, eval=F-----------------------------------------------------------------------------
-## set.seed(100)
-## fit <- ergm(magnolia~edges+triangle,
-##             control=snctrl(MCMLE.effectiveSize=NULL))
+## ----magnolia-triangle, eval=F-------------------------------------------------------
+set.seed(100)
+fit <- ergm(magnolia~edges+triangle,
+            control=snctrl(MCMLE.effectiveSize=NULL))
 
 
-## ----magnolia-triangle-halted, eval=T, message=F, warning = F----------------------------------------------
+## ----magnolia-triangle-halted, eval=T, message=F, warning = F------------------------
 set.seed(1000)
 fit <- ergm(magnolia~edges+triangle, 
             control=snctrl(MCMLE.maxit=2,MCMLE.effectiveSize=NULL))
 
 
-## ----mcmc-diagnostics, eval=T, results='hide', fig.show='asis'---------------------------------------------
+## ----mcmc-diagnostics, eval=T, results='hide', fig.show='asis'-----------------------
 mcmc.diagnostics(fit)
 
 
-## ----gwesp, message = F, warning = F-----------------------------------------------------------------------
+## ----gwesp, message = F, warning = F-------------------------------------------------
 set.seed(10101)
 fit <- ergm(magnolia~edges+gwesp(0.25, fixed=T), 
             control=snctrl(MCMC.interval = 10000),
             verbose=T)
 
 
-## ----gwesp-diagnostics-------------------------------------------------------------------------------------
+## ----gwesp-diagnostics---------------------------------------------------------------
 mcmc.diagnostics(fit)
 
